ec82ffae5efeb767836fd88fe1d72ba144ab3d01
Tony <abroodie@gmail.com>
2019-09-28 13:39:21 +0100

    added batch handlers module

---
+++ src/SFA.DAS.Payments.Monitoring.Jobs.JobService/Infrastructure/Ioc/BatchMessageHandlersModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using SFA.DAS.Payments.Application.Messaging;
+using SFA.DAS.Payments.Monitoring.Jobs.JobService.Handlers;
+using SFA.DAS.Payments.Monitoring.Jobs.Messages.Commands;
+
+namespace SFA.DAS.Payments.Monitoring.Jobs.JobService.Infrastructure.Ioc
+{
+    public class BatchMessageHandlersModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<RecordJobMessageProcessingStatusBatchHandler>()
+                .As<IHandleMessageBatches<RecordJobMessageProcessingStatus>>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<RecordEarningsJobBatchHandler>()
+                .As<IHandleMessageBatches<RecordEarningsJob>>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<RecordEarningsJobAdditionalMessageBatchHandler>()
+                .As<IHandleMessageBatches<RecordEarningsJobAdditionalMessages>>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
