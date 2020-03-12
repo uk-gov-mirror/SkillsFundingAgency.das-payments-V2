c548cc8a2db805feade30dc78a0285ad2d5fc702
Tony <abroodie@gmail.com>
2020-03-12 16:04:28 +0000

    Added handler for batches of CalculatedRequiredLevyAmount events

---
+++ src/SFA.DAS.Payments.FundingSource.LevyTransactionStorageService/Handlers/RecordLevyTransactionBatchHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Messaging;
+using SFA.DAS.Payments.RequiredPayments.Messages.Events;
+
+namespace SFA.DAS.Payments.FundingSource.LevyTransactionStorageService.Handlers
+{
+    public class RecordLevyTransactionBatchHandler : IHandleMessageBatches<CalculatedRequiredLevyAmount>
+    {
+        private readonly IPaymentLogger logger;
+
+        public RecordLevyTransactionBatchHandler(IPaymentLogger logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Handle(IList<CalculatedRequiredLevyAmount> messages, CancellationToken cancellationToken)
+        {
+            logger.LogInfo($"Received {messages.Count} messages");
+        }
+    }
+}
---
+++ src/SFA.DAS.Payments.FundingSource.LevyTransactionStorageService/Infrastructure/Ioc/LevyTransactionStorageServiceModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using SFA.DAS.Payments.Application.Infrastructure.Ioc;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.Application.Infrastructure.Telemetry;
+using SFA.DAS.Payments.Application.Messaging;
+using SFA.DAS.Payments.Core.Configuration;
+using SFA.DAS.Payments.FundingSource.LevyTransactionStorageService.Handlers;
+using SFA.DAS.Payments.ServiceFabric.Core;
+
+namespace SFA.DAS.Payments.FundingSource.LevyTransactionStorageService.Infrastructure.Ioc
+{
+    public class LevyTransactionStorageServiceModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c =>
+                {
+                    var appConfig = c.Resolve<IApplicationConfiguration>();
+                    var configHelper = c.Resolve<IConfigurationHelper>();
+                    return new ServiceBusBatchCommunicationListener(configHelper.GetConnectionString("MonitoringServiceBusConnectionString"),
+                        appConfig.EndpointName,
+                        appConfig.FailedMessagesQueue,
+                        c.Resolve<IPaymentLogger>(),
+                        c.Resolve<IContainerScopeFactory>(),
+                        c.Resolve<ITelemetry>());
+                })
+                .As<IServiceBusBatchCommunicationListener>()
+                .SingleInstance();
+
+            builder.RegisterType<RecordLevyTransactionBatchHandler>()
+                .As<IHandleMessageBatches<RecordLevyTransactionBatchHandler>>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
